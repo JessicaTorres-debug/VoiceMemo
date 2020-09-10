e66264b5d5adf4bbd6b65c5e2ac0c64b0c46b6b9
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-09-10 12:29:03 -0500

    Create the start and stop functionality for the stream audio recording. Chunk the audio data as it is created and collect them to be turned into Blob. Disable the buttons as they are used

--- popup.js
+++ popup.js
@@ -12,15 +12,51 @@ let stream,
   media;
 
 
-  streamBtn.onclick = e => {
-    let mediaOptions = {
-        audio: {
-          tag: 'audio',
-          type: 'audio/webm',
-          ext: '.mp3',
-          gUM: { audio: true }
+streamBtn.onclick = e => {
+  let mediaOptions = {
+    audio: {
+      tag: 'audio',
+      type: 'audio/webm',
+      ext: '.mp3',
+      gUM: { audio: true }
+    }
+  };
+
+  media = mediaOptions.audio;
+
+  navigator.mediaDevices.getUserMedia(media.gUM)
+    .then(_stream => {
+      stream = _stream;
+
+      id('btns').style.display = 'inherit';
+      start.removeAttribute('disabled');
+
+      recorder = new MediaRecorder(stream);
+
+      recorder.ondataavailable = e => {
+        chunks.push(e.data);
+        if (recorder.state == 'inactive') {
+          counter++;
+          takeName();
         }
-    };
-  
-    media = mediaOptions.audio;
-  }
+      };
+
+      log('Media successfully received');
+    })
+    .catch(log);
+}
+
+
+start.onclick = e => {
+  start.disabled = true;
+  stop.removeAttribute('disabled');
+  chunks = [];
+  recorder.start();
+}
+
+
+stop.onclick = e => {
+  stop.disabled = true;
+  recorder.stop();
+  start.removeAttribute('disabled');
+}
